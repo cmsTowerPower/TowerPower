4525855fddd595800ed5be6be5989b8e719b11da
davidm15 <david.mischak@edu.fh-joanneum.at>
2019-05-09 10:22:41 +0200

    Basic playground setup

---
+++ Assets/ControllerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+using Leap;
+
+public class ControllerMove : MonoBehaviour
+{
+
+    private Controller controller;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        controller = new Controller();
+        
+    }
+
+}
