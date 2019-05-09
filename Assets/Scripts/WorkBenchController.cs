3def4737de7a33b180a159c828a530d8809eb643
MartinHaring <martin.haring1106@gmail.com>
2019-05-09 11:37:16 +0200

    Add object spawning functionality

---
+++ Assets/Scripts/WorkBenchController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WorkBenchController : MonoBehaviour
+{
+    public GameObject spawnPoint;
+
+    private List<GameObject> objects = new List<GameObject>();
+
+    void Start() {
+        objects = Resources.LoadAll<GameObject>("Objects").ToList();
+    }
+
+    void Update() {
+        if (PlayerPrefs.GetInt("readyToSpawn") == 1) spawn();
+    }
+
+    void spawn() {
+        GameObject obj = objects[Random.Range(0, objects.Count)].gameObject;
+        Instantiate(obj, spawnPoint.transform.position, Quaternion.identity);
+        PlayerPrefs.SetInt("readyToSpawn", 0);
+    }
+}
