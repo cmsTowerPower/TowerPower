be07837bea3a75d49ab1d25dc9e29d428161d745
MartinHaring <martin.haring1106@gmail.com>
2019-05-18 19:16:07 +0200

    Fix everything the version change broke

---
+++ Assets/Scripts/DemoController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoController : MonoBehaviour {
+    void Awake() {
+        PlayerPrefs.SetInt("readyToSpawn", 1);
+    }
+}
